2c6c6f53ce703f06a07cd551b2d6d4d83b3c37f5
Steve Wilson <steverw@umich.edu>
2024-11-01 23:13:58 -0400

    Update score.py
    removing some super embarrassing print statements in the scoring function 😅

--- competition_bundle/scoring_program/score.py
+++ competition_bundle/scoring_program/score.py
@@ -12,27 +12,12 @@ if __name__ == "__main__":
     # set up the directories
     input_dir = argv[1]
     output_dir = argv[2]
-    print(input_dir, output_dir)
-    print(os.listdir(input_dir))
     os.makedirs(output_dir, exist_ok=True)
-    print(os.listdir(output_dir))
-    res_dir = os.path.join(input_dir, 'res')
-    ref_dir = os.path.join(input_dir, 'ref')
-    print(os.listdir(res_dir))
-    print(os.listdir(ref_dir), flush=True)
-
 
     # set the file paths
     score_file = os.path.join(output_dir, 'scores.txt')
     pred_file = os.path.join(input_dir, 'res', 'preds.txt')
     ground_truth_file = os.path.join(input_dir, 'ref', "labels.txt")
-
-    print(score_file, pred_file, ground_truth_file)
-    print("===",flush=True)
-    sys.stdout.flush()
-
-    with open("output2.txt",'w') as out:
-        print(str(os.listdir(res_dir)))
 
     # load the data
     preds = [line.strip() for line in open(pred_file).readlines()]
